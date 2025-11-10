686266b981ebd45f4c6d56ed43775abbdbc27770
craig <craighawkins@Craigs-MacBook-Air.local>
2025-11-10 12:23:31 +1100

    new

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use(express.json());
+
+app.get('/items', (req, res) => {
+    res.send('GET request');
+});
+
+app.post('/items', (req, res) => {
+    res.send('POST request');
+});
+
+app.put('/items/:id', (req, res) => {
+    res.send('PUT request');
+});
+
+app.delete('/items/:id', (req, res) => {
+    res.send('DELETE request');
 });
 
 app.listen(3000, function () {
